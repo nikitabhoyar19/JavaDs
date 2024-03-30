ff23faf7e784e51f230951e8df51b42e0d68a428
Nikita Bhoyar <nikitambhoyar@gmail.com>
2024-03-30 23:44:46 +0530

    sort characters by frequency uing priority queue

--- DsQusetions/src/String/SortCharactersByFrequency.java
+++ DsQusetions/src/String/SortCharactersByFrequency.java
@@ -16,15 +16,31 @@ public class SortCharactersByFrequency {
             map.put(c, map.getOrDefault(c, 0) + 1);
         }
 
-        List<Character> chars = new ArrayList<>(map.keySet());
-        chars.sort((a, b) -> map.get(b) - map.get(a));
+//        List<Character> chars = new ArrayList<>(map.keySet());
+//        chars.sort((a, b) -> map.get(b) - map.get(a));
+//        StringBuilder sb = new StringBuilder();
+//        for (Character ch : chars) {
+//            for(int i = 0; i < map.get(ch); i++) {
+//                sb.append(ch);
+//            }
+//        }
+
+        // Using Priority Queue
         StringBuilder sb = new StringBuilder();
-        for (Character ch : chars) {
-            for(int i = 0; i < map.get(ch); i++) {
+        PriorityQueue<Map.Entry<Character, Integer>> pq = new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
+
+        pq.addAll(map.entrySet());
+
+        while(!pq.isEmpty()) {
+            Map.Entry<Character, Integer> entry = pq.poll();
+            char ch = entry.getKey();
+            int count = entry.getValue();
+            for(int i = 0; i < count; i++) {
                 sb.append(ch);
             }
+            // you can use this instead of for :
+            // sb.append(String.valueOf(ch).repeat(Math.max(0, count)));
         }
-        
         return sb.toString();
     }
 }
